f7f85cba609c01b441f8aa2523e07b8b75f3d6df
Victor Liu <victorliu716@gmail.com>
2020-04-29 19:31:24 -0400

    travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '785131520204-uvbg15td61co0um8m1ic11q6me9u5f17.apps.googleusercontent.com',
+  googleClientSecret: 'iagEl9XSJMkSh9rWX0kyFrTo',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    '785131520204-uvbg15td61co0um8m1ic11q6me9u5f17.apps.googleusercontent.com',
+  googleClientSecret: 'iagEl9XSJMkSh9rWX0kyFrTo',
+  mongoURI:
+    'mongodb://victor:victor123@advanced-node-shard-00-00-jl27p.mongodb.net:27017,advanced-node-shard-00-01-jl27p.mongodb.net:27017,advanced-node-shard-00-02-jl27p.mongodb.net:27017/test?ssl=true&replicaSet=advanced-node-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
